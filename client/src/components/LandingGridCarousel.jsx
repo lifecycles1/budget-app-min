7ee15eebd37701b62c32c5c72d992e96fe84459d
lifecycles1 <raichev89@gmail.com>
2024-05-21 22:33:09 +0100

    upd

--- client/src/components/LandingGridCarousel.jsx
+++ client/src/components/LandingGridCarousel.jsx
@@ -1,82 +1,67 @@
 import React from "react";
-import Carousel from "react-grid-carousel";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const LandingGridCarousel = () => {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 12,
+      slidesToSlide: 1,
+    },
+  };
+
   return (
     <div className="mt-7">
-      <Carousel cols={6} rows={1} gap={1} autoplay={7000} loop>
-        <Carousel.Item>
-          <div className="ml-5 hover:cursor-pointer">
-            <img className="carousel-image" width="75" height="75" src="https://img.icons8.com/color/48/rainy-weather.png" alt="rainy-weather" />
-            <div className="w-20 text-teal-600 font-bold">Plan for Emergencies</div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="hover:cursor-pointer">
-            <img className="carousel-image" width="75" height="75" src="https://img.icons8.com/emoji/48/calendar-emoji.png" alt="calendar-emoji" />
-            <div className="w-44 text-teal-600 font-bold -ml-8">Stop Living Paycheck to Paycheck</div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="hover:cursor-pointer">
-            <img width="75" height="75" src="https://img.icons8.com/clouds/100/scorpion-scissors.png" alt="scorpion-scissors" />
-            <div className="w-44 text-teal-600 font-bold -ml-4">Get Out of Debt</div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="hover:cursor-pointer">
-            <img width="75" height="75" src="https://img.icons8.com/color/48/sunbathe.png" alt="sunbathe" />
-            <div className="w-44 text-teal-600 font-bold -ml-5">Go on Vacation</div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="hover:cursor-pointer">
-            <img width="75" height="75" src="https://img.icons8.com/clouds/100/apple-calculator.png" alt="apple-calculator" />
-            <div className="w-44 text-teal-600 font-bold -ml-8">Budget like an Adult</div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="ml-7 hover:cursor-pointer">
-            <img width="75" height="75" src="https://img.icons8.com/bubbles/50/sedan.png" alt="sedan" />
-            <div className="w-44 text-teal-600 font-bold">Save for Car</div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="ml-6 hover:cursor-pointer">
-            <img width="75" height="75" src="https://img.icons8.com/cute-clipart/64/wedding-cake.png" alt="wedding-cake" />
-            <div className="w-44 text-teal-600 font-bold -ml-6">Getting Married</div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="ml-6 hover:cursor-pointer">
-            <img width="75" height="75" src="https://img.icons8.com/dusk/64/couple-man-woman.png" alt="couple-man-woman" />
-            <div className="w-44 text-teal-600 font-bold -ml-10">Budgeting as a Couple</div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="ml-8 hover:cursor-pointer">
-            <img width="75" height="75" src="https://img.icons8.com/bubbles/50/baby.png" alt="baby" />
-            <div className="w-44 text-teal-600 font-bold ml-4">Baby</div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="ml-4 hover:cursor-pointer">
-            <img width="75" height="75" src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/external-kids-modelling-agency-flaticons-lineal-color-flat-icons-3.png" alt="external-kids-modelling-agency-flaticons-lineal-color-flat-icons-3" />
-            <div className="w-44 text-teal-600 font-bold ml-5">Kids</div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="ml-3 hover:cursor-pointer">
-            <img width="75" height="75" src="https://img.icons8.com/clouds/100/cottage.png" alt="cottage" />
-            <div className="w-44 text-teal-600 font-bold -ml-2">Buy a House</div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="ml-3 hover:cursor-pointer">
-            <img width="75" height="75" src="https://img.icons8.com/emoji/48/graduation-cap-emoji.png" alt="graduation-cap-emoji" />
-            <div className="w-44 text-teal-600 font-bold -ml-5">Save for College</div>
-          </div>
-        </Carousel.Item>
+      <Carousel className="flex h-40" responsive={responsive} additionalTransfrom={0} arrows autoPlay autoPlaySpeed={3000} centerMode={true} infinite minimumTouchDrag={80}>
+        <div className="w-20 h-40 flex flex-col items-center justify-center">
+          <img src="https://img.icons8.com/color/48/rainy-weather.png" alt="rainy-weather" />
+          <div className="text-teal-600 font-bold">Emergencies</div>
+        </div>
+        <div className="w-20 h-40 flex flex-col items-center justify-center">
+          <img src="https://img.icons8.com/emoji/48/calendar-emoji.png" alt="calendar-emoji" />
+          <div className="text-teal-600 font-bold">Paychecks</div>
+        </div>
+        <div className="w-20 h-40 flex flex-col items-center justify-center">
+          <img width={55} src="https://img.icons8.com/clouds/100/scorpion-scissors.png" />
+          <div className="text-teal-600 font-bold">Debts</div>
+        </div>
+        <div className="w-20 h-40 flex flex-col items-center justify-center">
+          <img src="https://img.icons8.com/color/48/sunbathe.png" alt="sunbathe" />
+          <div className="text-teal-600 font-bold">Vacations</div>
+        </div>
+        <div className="w-20 h-40 flex flex-col items-center justify-center">
+          <img width={53} src="https://img.icons8.com/clouds/100/apple-calculator.png" alt="apple-calculator" />
+          <div className="text-teal-600 font-bold">Budgets</div>
+        </div>
+        <div className="w-20 h-40 flex flex-col items-center justify-center">
+          <img src="https://img.icons8.com/bubbles/50/sedan.png" alt="sedan" />
+          <div className="text-teal-600 font-bold">Cars</div>
+        </div>
+        <div className="w-20 h-40 flex flex-col items-center justify-center">
+          <img src="https://img.icons8.com/cute-clipart/64/wedding-cake.png" alt="wedding-cake" />
+          <div className="text-teal-600 font-bold">Marriages</div>
+        </div>
+        <div className="w-20 h-40 flex flex-col items-center justify-center">
+          <img src="https://img.icons8.com/dusk/64/couple-man-woman.png" alt="couple-man-woman" />
+          <div className="text-teal-600 font-bold">Couples</div>
+        </div>
+        <div className="w-20 h-40 flex flex-col items-center justify-center">
+          <img src="https://img.icons8.com/bubbles/50/baby.png" alt="baby" />
+          <div className="text-teal-600 font-bold">Babies</div>
+        </div>
+        <div className="w-20 h-40 flex flex-col items-center justify-center">
+          <img src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/external-kids-modelling-agency-flaticons-lineal-color-flat-icons-3.png" alt="external-kids-modelling-agency-flaticons-lineal-color-flat-icons-3" />
+          <div className="text-teal-600 font-bold">Kids</div>
+        </div>
+        <div className="w-20 h-40 flex flex-col items-center justify-center">
+          <img width={55} src="https://img.icons8.com/clouds/100/cottage.png" alt="cottage" />
+          <div className="text-teal-600 font-bold">House</div>
+        </div>
+        <div className="w-20 h-40 flex flex-col items-center justify-center">
+          <img src="https://img.icons8.com/emoji/48/graduation-cap-emoji.png" alt="graduation-cap-emoji" />
+          <div className="text-teal-600 font-bold">College</div>
+        </div>
       </Carousel>
     </div>
   );
--- client/src/components/LandingTextCarousel.jsx
+++ client/src/components/LandingTextCarousel.jsx
@@ -1,29 +1,20 @@
 import React from "react";
-import Carousel from "react-grid-carousel";
 
 const LandingTextCarousel = () => {
   return (
-    <div className="mt-20">
-      <Carousel cols={1} rows={1} gap={1} autoplay={7000} loop>
-        <Carousel.Item>
-          <div className="hover:cursor-pointer ml-44">
-            <div className=" text-teal-600 font-bold text-2xl">5 Money Lessons My Mom Taught Me</div>
-            <div className="text-teal-600 font-medium">Hear Chi-En reflect on valuable lessons her mom taught her.</div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="hover:cursor-pointer ml-44">
-            <div className="text-teal-600 font-bold text-2xl">Money Tips for Recent Grads</div>
-            <div className="text-teal-600 font-medium">See 3 tips to help you create an awesome money plan.</div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="hover:cursor-pointer ml-44">
-            <div className="text-teal-600 font-bold text-2xl">Vacations Are Better When You Can Afford Them</div>
-            <div className="text-teal-600 font-medium">Learn why it's important to save for your vacation in advance.</div>
-          </div>
-        </Carousel.Item>
-      </Carousel>
+    <div className="mt-20 flex items-center justify-evenly">
+      <div className="w-72">
+        <div className="text-teal-600 font-bold text-2xl">5 Money Lessons My Mom Taught Me</div>
+        <div className="text-teal-600 font-medium">Hear Chi-En reflect on valuable lessons her mom taught her.</div>
+      </div>
+      <div className="w-72">
+        <div className="text-teal-600 font-bold text-2xl">Money Tips for Recent Grads</div>
+        <div className="text-teal-600 font-medium">See 3 tips to help you create an awesome money plan.</div>
+      </div>
+      <div className="w-72">
+        <div className="text-teal-600 font-bold text-2xl">Vacations Are Better When You Can Afford Them</div>
+        <div className="text-teal-600 font-medium">Learn why it's important to save for your vacation in advance.</div>
+      </div>
     </div>
   );
 };
